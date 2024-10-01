7697851595e45d8ffad0acf5d482c321fb5d2de1
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2024-10-01 20:54:38 +0530

    add adapters

---
+++ app/src/main/java/com/example/lastlastrecipe/adapters/RecipeAdapter.java
@@ -0,0 +1,70 @@
+package com.example.lastlastrecipe.adapters;
+
+import static com.example.lastlastrecipe.databinding.ItemRecipeBinding.*;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.lastlastrecipe.R;
+import com.example.lastlastrecipe.RecipeDetailsActivity;
+import com.example.lastlastrecipe.databinding.ItemRecipeBinding;
+import com.example.lastlastrecipe.models.Recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeHolder> {
+    List<Recipe> recipeList = new ArrayList<>();
+
+    public void setRecipeList(List<Recipe> recipeList) {
+        this.recipeList = recipeList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public RecipeAdapter.RecipeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new RecipeHolder(inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeAdapter.RecipeHolder holder, int position) {
+        Recipe recipe = recipeList.get(position);
+        holder.onBind(recipe);
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipeList.size();
+    }
+
+    public static class RecipeHolder extends RecyclerView.ViewHolder {
+        ItemRecipeBinding binding;
+        public RecipeHolder(@NonNull ItemRecipeBinding itemView) {
+            super(itemView.getRoot());
+            binding = itemView;
+        }
+
+        public void onBind(Recipe recipe) {
+            Glide
+                    .with(binding.getRoot().getContext())
+                    .load(recipe.getImage())
+                    .centerCrop()
+                    .placeholder(R.mipmap.ic_launcher)
+                    .into(binding.bgImgRecipe);
+            binding.tvRecipeName.setText(recipe.getName());
+
+            binding.getRoot().setOnClickListener(view -> {
+                Intent intent = new Intent(binding.getRoot().getContext(), RecipeDetailsActivity.class);
+                intent.putExtra("recipe", recipe);
+                binding.getRoot().getContext().startActivity(intent);
+            });
+        }
+    }
+}
